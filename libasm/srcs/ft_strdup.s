e8de18e8aa48a2fd987c56dc9992a6b1ed9c493c
Archibald THIRION <athirion@made-f0Ar7s1.clusters.42paris.fr>
2023-09-11 12:06:53 +0200

    ft_strdup done

--- libasm/srcs/ft_strdup.s
+++ libasm/srcs/ft_strdup.s
@@ -8,4 +8,17 @@ global ft_strdup
 
 section .text
     ft_strdup:
+        call    ft_strlen
+        inc     rax
+        push    rdi
+        mov     rdi, rax
+        call    malloc wrt ..plt
+        cmp     rdi, 0
+        je      error
+        mov     rdi, rax
+        pop     rsi
+        call    ft_strcpy
+        ret
+
+    error:
         ret
