94edfe0d55041675edf8da476c4819415f2b0ec1
Archibald Thirion <archibald@archibalds-mbp.home>
2023-09-21 18:47:48 +0200

    strcmp test in main.c

--- libasm/test/main.c
+++ libasm/test/main.c
@@ -2,8 +2,8 @@
 #include <errno.h>
 #include <stdio.h>
 
-size_t ft_strlen(const char *s);
-
+size_t  ft_strlen(const char *s);
+int     ft_strcmp(const char *s1, const char *s2);
 
 void test_strlen(const char *s) {
 
@@ -20,10 +20,47 @@ void test_strlen(const char *s) {
     }
 }
 
+void test_strcmp(const char *s1, const char *s2) {
+
+    int ret1 = ft_strcmp(s1, s2);
+    int err1 = errno;
+
+    int ret2 = strcmp(s1, s2);
+    int err2 = errno;
+
+    if (ret1 == ret2 && err1 == err2) {
+        printf("OK\n");
+    } else {
+        printf("KO\n");
+    }
+}
+
 int		main(void)
 {
+    printf("ft_strlen:\n\n");
+
     test_strlen("");
     test_strlen("heyho");
     test_strlen("heyyyyy!");
     test_strlen("Hello this should work");
+
+    printf("\n====================================\n");
+    printf("ft_strcmp:\n\n");
+
+    test_strcmp("Hello", "Hello");
+    test_strcmp("Hello!", "Hello");
+    test_strcmp("Hello", "Hello!");
+    test_strcmp("He!!o", "Hello");
+    test_strcmp("Hello", "He!!o");
+
+    test_strcmp("Hello\tHi", "Hello\tHi");
+    test_strcmp("Hello\200Hi", "Hello\0Hi");
+    test_strcmp("Hello\0Hi", "Hello\200Hi");
+    test_strcmp("Hello\200Hi", "Hello\200Hi");
+    test_strcmp("Hello\0Hi", "Hello\0Hi");
+
+    test_strcmp("This string is longer", "This string is longer");
+    test_strcmp("You might pass all these tests", "Or not");
+    test_strcmp("or not", "Or not");
+    test_strcmp("or noT", "or not");
 }
