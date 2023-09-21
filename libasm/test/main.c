36a4a1598e2dee9406436fa8ab0d1b172e4f9ece
Archibald Thirion <archibald@archibalds-mbp.home>
2023-09-21 19:54:33 +0200

    draft of all tests is done

--- libasm/test/main.c
+++ libasm/test/main.c
@@ -2,10 +2,14 @@
 #include <errno.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 size_t  ft_strlen(const char *s);
 int     ft_strcmp(const char *s1, const char *s2);
-size_t ft_write(int fd, const char *s, size_t size);
+size_t  ft_write(int fd, const char *s, size_t size);
+size_t  ft_read(int fd, void *buf, size_t nbyte);
+char *  ft_strcpy(char * dst, const char * src);
+char *  ft_strdup(const char *s1);
 
 void test_strlen(const char *s) {
 
@@ -58,6 +62,67 @@ void test_write(int fd, const char *s, size_t size) {
     }
 }
 
+void test_read(int fd, void *buf, size_t nbyte) {
+
+        errno = 0;
+        size_t ret1 = ft_read(fd, buf, nbyte);
+        int err1 = errno;
+        size_t ret2 = read(fd, buf, nbyte);
+        int err2 = errno;
+        if (fd < 0 || fd > 2)
+            printf("wrong fd\n");
+        if (!buf)
+            printf("buf is NULL\n");
+        if (nbyte < 1)
+            printf("wrong size\n");
+        printf("err1 = %d && err2 = %d\n", err1, err2);
+        if (ret1 == ret2 && err1 == err2) {
+            printf("OK\n");
+        } else {
+            printf("KO\n");
+        }
+}
+
+void test_strcpy(char * dst, const char * src) {
+
+        char *dst1 = malloc(sizeof(char) * 100);
+        char *dst2 = malloc(sizeof(char) * 100);
+
+        strcpy(dst1, dst);
+        strcpy(dst2, dst);
+
+        char *ret1 = ft_strcpy(dst1, src);
+        int err1 = errno;
+
+        char *ret2 = strcpy(dst2, src);
+        int err2 = errno;
+
+        if (!strcmp(ret1, dst1) && !strcmp(ret2, dst2) && err1 == err2) {
+            printf("OK\n");
+        } else {
+            printf("KO\n");
+        }
+        free(dst1);
+        free(dst2);
+}
+
+void test_strdup(const char *s1) {
+
+        char *ret1 = ft_strdup(s1);
+        int err1 = errno;
+
+        char *ret2 = strdup(s1);
+        int err2 = errno;
+
+        if (ret1 && ret2 && !strcmp(ret1, ret2) && err1 == err2) {
+            printf("OK\n");
+        } else {
+            printf("KO\n");
+        }
+        free(ret1);
+        free(ret2);
+}
+
 int		main(void)
 {
     printf("ft_strlen:\n\n");
@@ -103,4 +168,42 @@ int		main(void)
     test_write(1, NULL, 5);
     test_write(1, NULL, -1);
     test_write(1, "A longer sentence!\n", 19);
+
+//    printf("\n====================================\n");
+//    printf("\nft_read:\n\n");
+//
+//    test_read(9, 9, 5);
+//    test_read(9, 9, 9);
+//
+//    test_read(rdfd, rdfd_true, 5);
+//    test_read(rdfd, rdfd_true, 1);
+//    test_read(rdfd, rdfd_true, 9);
+//    test_read(rdfd, rdfd_true, 7);
+//    test_read(rdfd, rdfd_true, 18);
+//    test_read(rdfd, rdfd_true, 30);
+//
+//    test_read(wrfd, wrfd, 5);
+//    test_read(wrfd, wrfd, 1);
+//    test_read(wrfd, wrfd, 9);
+//    test_read(wrfd, wrfd, 7);
+//    test_read(wrfd, wrfd, 18);
+//    test_read(wrfd, wrfd, 30);
+
+    printf("\n====================================\n");
+    printf("\nft_strcpy:\n\n");
+
+    test_strcpy("", "");
+    test_strcpy("ah", "");
+    test_strcpy("hey", "ho");
+    test_strcpy("Ok this is more normal", "Yeayah");
+    test_strcpy("Ok this is more normal", "as it is written, normal.");
+
+    printf("\n====================================\n");
+    printf("\nft_strdup:\n\n");
+
+    test_strdup("");
+    test_strdup("heyho");
+    test_strdup("heyyyyy!");
+    test_strdup("Hello this should work");
+
 }
