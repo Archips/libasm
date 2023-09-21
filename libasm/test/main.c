7d1997cb0e6c57f26088c633f50e7a15cebf8771
Archibald Thirion <archibald@archibalds-mbp.home>
2023-09-21 18:26:15 +0200

    test main.c

---
+++ libasm/test/main.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <errno.h>
+
+
+void test_strlen(const char *s) {
+
+    size_t ret1 = ft_strlen(s);
+    int err1 = errno;
+
+    size_t ret2 = strlen(s);
+    int err2 = errno;
+
+    if (ret1 == ret2 && err1 == err2) {
+        printf("OK\n");
+    } else {
+        printf("KO\n");
+    }
+}
+
+int		main(void)
+{
+    test_strlen("");
+    test_strlen("heyho");
+    test_strlen("heyyyyy!");
+    test_strlen("Hello this should work");
+}
